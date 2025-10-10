8fb74c6285a94ad51c0ca611e8714e77464bd991
Pujan Khunt <pujankhunt2412@gmail.com>
2025-10-10 11:04:25 +0530

    Implement a thread pool using Executor Service replacing individual threads per client

--- com/http/Main.java
+++ com/http/Main.java
@@ -7,6 +7,7 @@ import com.http.server.HttpServer;
 public class Main {
     private static int PORT = 8080;
     private static String HOST = "127.0.0.1";
+    private static int THREAD_POOL_SIZE = 10;
 
     public static void main(String[] args) {
         if (args.length > 0) {
@@ -15,12 +16,15 @@ public class Main {
         if (args.length > 1) {
             HOST = args[1];
         }
+        if(args.length > 2) {
+            THREAD_POOL_SIZE = Integer.parseInt(args[2]);
+        }
 
         Application app = new Application();
 
         Router router = app.configureRouter();
 
-        HttpServer server = new HttpServer(PORT, HOST, router);
+        HttpServer server = new HttpServer(PORT, HOST, router, THREAD_POOL_SIZE);
 
         server.start();
     }
--- com/http/server/HttpServer.java
+++ com/http/server/HttpServer.java
@@ -5,6 +5,8 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.http.routing.Router;
 
@@ -13,11 +15,15 @@ public class HttpServer {
     private final int PORT;
     private final Router router;
     private final String HOST;
+    private final ExecutorService threadPool;
+    private final int THREAD_POOL_SIZE;
 
-    public HttpServer(int PORT, String HOST, Router router) {
+    public HttpServer(int PORT, String HOST, Router router, int THREAD_POOL_SIZE) {
         this.PORT = PORT;
         this.HOST = HOST;
         this.router = router;
+        this.THREAD_POOL_SIZE = THREAD_POOL_SIZE;
+        this.threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
     }
 
     public void start() {
@@ -35,19 +41,18 @@ public class HttpServer {
         try (
                 ServerSocket serverSocket = new ServerSocket(this.PORT, backlog, serverBindAddress);) {
             System.out.println("Server now listening for clients on: " + this.HOST + ":" + this.PORT);
+            System.out.println("Number of threads in the pool: " + this.THREAD_POOL_SIZE);
 
             while (true) {
                 // Receive from Socket object after client connection.
                 Socket clientSocket = serverSocket.accept();
 
-                // Assign new thread for handling client.
-                Thread handleClient = new Thread(new ClientConnection(clientSocket, router));
-
-                // Run the separate thread from the main thread.
-                handleClient.start();
+                threadPool.submit(new ClientConnection(clientSocket, router));
             }
         } catch (IOException e) {
             System.out.println("Server error occured due to I/O error. " + e.getMessage());
+        } finally {
+            threadPool.shutdown();
         }
 
         System.out.println("Server shutting down...");
